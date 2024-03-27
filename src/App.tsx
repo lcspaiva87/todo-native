8e1fb210b2250c377d80e900401c3e48286cb598
lucas paiva <lcspaiva87@gmail.com>
2024-03-27 00:04:26 -0300

    Adiciona configuração do eslint da Rocketseat e corrige estilos no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { styled, } from 'nativewind';
-const StyledView = styled(View)
-const StyledText = styled(Text)
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { styled } from "nativewind";
+const StyledView = styled(View);
+const StyledText = styled(Text);
 export default function App() {
-    const [text, setText] = React.useState('Hello, world! 🌍')
-  return (
-    <StyledView className="flex-1 items-center justify-center">
-    <StyledText className="text-slate-800">
-      Try editing me! 🎉
-    </StyledText>
-  </StyledView>
-  );
+    const [text, setText] = React.useState("Hello, world! 🌍");
+    return (
+        <StyledView className="flex-items-center justify-center">
+            <StyledText className="text-slate-800">
+                Try editing me! 🎉
+            </StyledText>
+        </StyledView>
+    );
 }
-
-
