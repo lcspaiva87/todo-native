d339c5544f690323449b47ec07aa80523749808c
lucas paiva <lcspaiva87@gmail.com>
2024-03-27 23:48:36 -0300

    fix: Add logo image to home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className="bg-gray-600 w-full h-full p-10">
+    <View className="bg-gray-600 w-full h-full ">
+      <View className="bg-gray-700 p-4 flex items-center py-16">
+        <Image source={require('../assets/Logo.png')} />
+      </View>
       <Text className="text-red-800">Try editing me! 🎉</Text>
     </View>
   )
