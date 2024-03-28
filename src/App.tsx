7fbabd75d319f7d479f1bcd74fcbc5e1f9a5def8
lucas paiva <lcspaiva87@gmail.com>
2024-03-27 22:09:47 -0300

    fix: adiciona configurações do Prettier e do Husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { styled } from "nativewind";
-const StyledView = styled(View);
-const StyledText = styled(Text);
+import React from 'react'
+import { Text, View } from 'react-native'
+import { styled } from 'nativewind'
+const StyledView = styled(View)
+const StyledText = styled(Text)
 export default function App() {
-    const [text, setText] = React.useState("Hello, world! 🌍");
-    return (
-        <StyledView className="flex-items-center justify-center">
-            <StyledText className="text-slate-800">
-                Try editing me! 🎉
-            </StyledText>
-        </StyledView>
-    );
+  return (
+    <StyledView className="flex-items-center justify-center">
+      <StyledText className="text-slate-800">Try editing me! 🎉</StyledText>
+    </StyledView>
+  )
 }
